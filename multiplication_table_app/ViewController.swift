b0f608bab655383901a38f0ccff4affffc106e96
epic650 <mshepard@macintosh.local>
2015-09-03 08:40:49 -0400

    Initial Commit

---
+++ multiplication_table_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  multiplication_table_app
+//
+//  Created by M Shepard on 9/3/2015.
+//  Copyright © 2015 Epic 650. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ multiplication_table_appTests/multiplication_table_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  multiplication_table_appTests.swift
+//  multiplication_table_appTests
+//
+//  Created by M Shepard on 9/3/2015.
+//  Copyright © 2015 Epic 650. All rights reserved.
+//
+
+import XCTest
+@testable import multiplication_table_app
+
+class multiplication_table_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
